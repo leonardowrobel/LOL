05be40090ca280166a5a6e0cbbbb38c6763d3acc
Leonardo Wrobel <leonardo.wrobel@gmail.com>
2020-10-19 14:14:33 -0300

    iniciando testes de persistencia

---
+++ src/test/java/com/persistence/TestePersistence.java
@@ -0,0 +1,47 @@
+package com.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author Leonardo
+ */
+@Entity
+@Table(name = "EVENTS")
+public class TestePersistence {
+
+    private Long id;
+    private String title;
+
+    public TestePersistence() {
+    }
+
+    public TestePersistence(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }    
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
